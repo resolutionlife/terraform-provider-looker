992595fd981fab5e47b008e0d090f273c3c1f3d1
Jess Somaiya <jessica.somaiya@atos.net>
2022-07-18 10:42:43 +0100

    update roles example

--- examples/roles/main.tf
+++ examples/roles/main.tf
@@ -1,7 +1,9 @@
 # TODO: extend this example to build the test_model_set and test_permission_set resources when they are supported
 
+provider "looker" {}
+
 resource "looker_roles" "test" {
   name              = "Test Role"
   model_set_id      = resource.test_model_set.id
-  permission_set_0d = resource.test_permission_set.id
+  permission_set_id = resource.test_permission_set.id
 }
