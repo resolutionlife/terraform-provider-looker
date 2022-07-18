eee0d473edf468f24ff5d4046e22e479df252345
Jess Somaiya <jessica.somaiya@atos.net>
2022-07-18 10:46:58 +0100

    fix example

--- examples/roles/main.tf
+++ examples/roles/main.tf
@@ -1,9 +1,9 @@
-# TODO: extend this example to build the test_model_set and test_permission_set resources when they are supported
+# TODO: extend this example to build the looker_model_set and looker_permission_set resources when they are supported
 
 provider "looker" {}
 
 resource "looker_roles" "test" {
   name              = "Test Role"
-  model_set_id      = resource.test_model_set.id
-  permission_set_id = resource.test_permission_set.id
+  model_set_id      = looker_model_set.test.id
+  permission_set_id = looker_permission_set.test.id
 }
