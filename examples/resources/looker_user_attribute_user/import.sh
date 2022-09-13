d3ec65f718541aeef0e9ff3b704a489a1cd99d48
Jess Somaiya <jessica.somaiya@atos.net>
2022-09-13 14:42:02 +0100

    add custom templates

---
+++ examples/resources/looker_user_attribute_user/import.sh
@@ -0,0 +1,4 @@
+A `looker_user_attribute_user` resource can be imported by delimiting the `user_attribute_id` and `user_id` with an underscore. E.g `{{user_attribute_id}}_{{user_id}}`. See the below syntax. 
+
+```
+terraform import looker_user_attribute_user.tina_secret_id {{secret_id}}_{{tina_user_id}}
---
+++ examples/resources/looker_user_roles/import.sh
@@ -0,0 +1,4 @@
+A `looker_user_roles` binding resource can be imported by delimiting the `user_id` and `role_ids` with an underscore. E.g `{{user_id}}_{{role_ids}}`. See the below syntax. 
+
+```
+terraform import looker_user_roles.tina_roles {{user_id}}_{{role_ids}}
