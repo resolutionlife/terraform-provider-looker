38b075342417931d949d0d410607af32d7e4f553
Mohamed Yusuf <vitzeno@gmail.com>
2022-09-15 16:25:26 +0100

    add tests for the looker_group_group resource

---
+++ looker/resource_looker_group_group_test.go
@@ -0,0 +1,126 @@
+package looker
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	client "github.com/looker-open-source/sdk-codegen/go/sdk/v4"
+	sdk "github.com/looker-open-source/sdk-codegen/go/sdk/v4"
+	"github.com/pkg/errors"
+	"github.com/resolutionlife/terraform-provider-looker/internal/conv"
+	"github.com/resolutionlife/terraform-provider-looker/internal/slice"
+)
+
+func init() {
+	// Add a sweeper to remove groups that have names starting with `test-acc`.
+	resource.AddTestSweepers("looker_group_group", &resource.Sweeper{
+		Name: "looker_group_group",
+		F: func(_ string) error {
+			c, err := newTestLookerSDK()
+			if err != nil {
+				return err
+			}
+
+			groups, err := c.SearchGroups(sdk.RequestSearchGroups{
+				Name: conv.PString("test-acc%"),
+			}, nil)
+			if err != nil {
+				return err
+			}
+
+			for _, g := range groups {
+				if _, err := c.DeleteGroup(*g.Id, nil); err != nil {
+					return err
+				}
+			}
+
+			// could user c.DeleteGroupFromGroup(), but deleting all groups should be sufficient
+			return nil
+		},
+	})
+}
+
+func TestAccLookerGroupGroup(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		ProviderFactories: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: `
+				resource "looker_group" "test_acc_parent" {
+					name = "test-acc-parent-group"
+				}
+
+				resource "looker_group" "test_acc_child" {
+					name = "test-acc-child-group"
+				}
+
+				resource "looker_group_group" "test_acc_group" {
+					parent_group_id = looker_group.test_acc_parent.id
+					group_id  = looker_group.test_acc_child.id
+				}
+				`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrSet("looker_group_group.test_acc_group", "parent_group_id"),
+					resource.TestCheckResourceAttrSet("looker_group_group.test_acc_group", "group_id"),
+					testAccGroupGroupBinding("looker_group.test_acc_parent", "looker_group.test_acc_child"),
+				),
+			},
+		},
+	})
+}
+
+func testAccGroupGroupBinding(parentGroupResource, childGroupResource string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		parentRes, ok := s.RootModule().Resources[parentGroupResource]
+		if !ok {
+			return errors.Errorf("Not found: %s", parentGroupResource)
+		}
+		if parentRes.Primary.ID == "" {
+			return errors.New("parent group ID is not set")
+		}
+
+		childRes, ok := s.RootModule().Resources[childGroupResource]
+		if !ok {
+			return errors.Errorf("Not found: %s", childGroupResource)
+		}
+		if childRes.Primary.ID == "" {
+			return errors.New("child group ID is not set")
+		}
+
+		client := testAccProvider.Meta().(*client.LookerSDK)
+
+		parentSubGroups, err := client.AllGroupGroups(parentRes.Primary.ID, "", nil)
+		if err != nil {
+			return errors.Wrapf(err, "failed to retrieve parent group with id: %v", parentRes.Primary.ID)
+		}
+
+		// TODO: get the flatten function working
+		groupIds := []string{}
+		for _, subGroup := range parentSubGroups {
+			groupIds = append(groupIds, *subGroup.Id)
+		}
+
+		if !slice.Contains(groupIds, childRes.Primary.ID) {
+			return errors.Errorf("parent group contains groups with ID %v does not contain group with ID %v", groupIds, childRes.Primary.ID)
+		}
+
+		return nil
+	}
+}
+
+/*
+  flattens a slice of struct to a slice of given struct fields
+  e.g.
+  flattenByField([{a: 1, b: 3}, {a: 5, b: 8}, {a: 7, b: 4}], a) => [1, 5, 7]
+  flattenByField([{a: 1, b: 3}, {a: 5, b: 8}, {a: 7, b: 4}], b) => [3, 8, 4]
+*/
+func flattenByField[T, S any](structSlice []T, fieldName string) []S {
+	var flattenedSlice []S
+	for _, s := range structSlice {
+		flattenedSlice = append(flattenedSlice, reflect.ValueOf(s).FieldByName(fieldName).Interface().(S))
+	}
+
+	return flattenedSlice
+}
