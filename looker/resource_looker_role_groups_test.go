b50835c4be6218d2441500f0312e6f3616a84f69
Mohamed Yusuf <vitzeno@gmail.com>
2022-09-22 12:00:35 +0100

    add functioning tests including update

--- looker/resource_looker_role_groups_test.go
+++ looker/resource_looker_role_groups_test.go
@@ -1,14 +1,16 @@
 package looker
 
 import (
+	"strings"
 	"testing"
 
-	"github.com/davecgh/go-spew/spew"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	sdk "github.com/looker-open-source/sdk-codegen/go/sdk/v4"
 	"github.com/pkg/errors"
 	"github.com/resolutionlife/terraform-provider-looker/internal/conv"
+	"github.com/resolutionlife/terraform-provider-looker/internal/slice"
 )
 
 func init() {
@@ -116,14 +118,46 @@ func TestAccLookerRoleGroups(t *testing.T) {
 				`,
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr("looker_role_groups.test_acc", "group_ids.#", "2"),
-					testAccRoleGroups("looker_role_groups.test_acc"),
+					testAccRoleGroups("looker_role_groups.test_acc", []string{"looker_group.test_acc_1", "looker_group.test_acc_2"}),
+				),
+			},
+			{
+				Config: `
+				resource "looker_permission_set" "test_acc" {
+					name        = "test-acc-permission-set"
+					permissions = ["access_data", "see_lookml", "see_lookml_dashboards"]
+				}
+
+				resource "looker_model_set" "test_acc" {
+					name   = "test-acc-model-set"
+					models = ["test_dataset_1", "test_dataset_2", "test_both_datasets"]
+				}
+
+				resource "looker_role" "test_acc" {
+					name              = "test-acc-role"
+					model_set_id      = looker_model_set.test_acc.id
+					permission_set_id = looker_permission_set.test_acc.id
+				}
+
+				resource "looker_group" "test_acc_1" {
+					name = "test-acc-group-1"
+				}
+
+				resource "looker_role_groups" "test_acc" {
+					role_id   = looker_role.test_acc.id
+					group_ids = [looker_group.test_acc_1.id]
+				} 
+				`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("looker_role_groups.test_acc", "group_ids.#", "1"),
+					testAccRoleGroups("looker_role_groups.test_acc", []string{"looker_group.test_acc_1"}),
 				),
 			},
 		},
 	})
 }
 
-func testAccRoleGroups(roleGroupResource string) resource.TestCheckFunc {
+func testAccRoleGroups(roleGroupResource string, groupResources []string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		roleGroupsRes, ok := s.RootModule().Resources[roleGroupResource]
 		if !ok {
@@ -133,19 +167,40 @@ func testAccRoleGroups(roleGroupResource string) resource.TestCheckFunc {
 			return errors.New("role groups ID is not set")
 		}
 
+		expectedGroupIds := []string{}
+		for _, groupResource := range groupResources {
+			groupRes, ok := s.RootModule().Resources[groupResource]
+			if !ok {
+				return errors.Errorf("Not found: %s", groupResource)
+			}
+			if groupRes.Primary.ID == "" {
+				return errors.New("group ID is not set")
+			}
+
+			expectedGroupIds = append(expectedGroupIds, groupRes.Primary.ID)
+		}
+
 		client := testAccProvider.Meta().(*sdk.LookerSDK)
 
-		roleGroups, err := client.RoleGroups(roleGroupsRes.Primary.ID, "", nil)
+		// role group binding resource id is NOT the id of the role resource
+		roleId := strings.Split(roleGroupsRes.Primary.ID, "_")
+		if len(roleId) < 2 {
+			diag.Errorf("invalid id, should be of the form <role_id>_<group_ids>")
+		}
+
+		roleGroups, err := client.RoleGroups(roleId[0], "", nil)
 		if err != nil {
 			return errors.Wrapf(err, "failed to retrieve role group with id: %v", roleGroupsRes.Primary.ID)
 		}
 
-		var groupIds []string
+		groupIds := []string{}
 		for _, roleGroup := range roleGroups {
-			groupIds = append(groupIds, *roleGroup.ExternalGroupId)
+			groupIds = append(groupIds, *roleGroup.Id)
 		}
 
-		spew.Dump(groupIds)
+		if !slice.UnorderedEqual(groupIds, expectedGroupIds) {
+			return errors.Errorf("groups in role do not match expected: %v actual: %v", expectedGroupIds, groupIds)
+		}
 
 		return nil
 	}
