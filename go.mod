cee5c2fa2d96186a3c580d706df4d6d050378d3c
Mohamed Yusuf <vitzeno@gmail.com>
2022-09-22 10:53:52 +0100

    Test: Resource looker_model_set (#32)
    * add model set tests
    
    also fix duplicate import in group group test
    
    * update naming
    
    * add update test

--- go.mod
+++ go.mod
@@ -23,7 +23,7 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fatih/color v1.13.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/google/go-cmp v0.5.8 // indirect
+	github.com/google/go-cmp v0.5.8
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-checkpoint v0.5.0 // indirect
@@ -65,9 +65,10 @@ require (
 	github.com/vmihailenco/tagparser v0.1.1 // indirect
 	github.com/zclconf/go-cty v1.10.0 // indirect
 	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
+	golang.org/x/exp v0.0.0-20220916125017-b168a2c6b86b
 	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
 	golang.org/x/oauth2 v0.0.0-20220411215720-9780585627b5 // indirect
-	golang.org/x/sys v0.0.0-20220627191245-f75cf1eec38b // indirect
+	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/appengine v1.6.6 // indirect
 	google.golang.org/genproto v0.0.0-20200904004341-0bd0a958aa1d // indirect
---
+++ looker/resource_looker_model_set_test.go
@@ -0,0 +1,99 @@
+package looker
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	sdk "github.com/looker-open-source/sdk-codegen/go/sdk/v4"
+	"github.com/pkg/errors"
+	"github.com/resolutionlife/terraform-provider-looker/internal/conv"
+	"github.com/resolutionlife/terraform-provider-looker/internal/slice"
+)
+
+func init() {
+	// Add a sweeper to remove model sets that have names starting with `test-acc`.
+	resource.AddTestSweepers("looker_model_set", &resource.Sweeper{
+		Name: "looker_model_set",
+		F: func(_ string) error {
+			c, err := newTestLookerSDK()
+			if err != nil {
+				return err
+			}
+
+			modelSets, err := c.SearchModelSets(sdk.RequestSearchModelSets{
+				Name: conv.PString("test-acc%"),
+			}, nil)
+			if err != nil {
+				return err
+			}
+
+			for _, modelSet := range modelSets {
+				if _, err := c.DeleteModelSet(*modelSet.Id, nil); err != nil {
+					return err
+				}
+			}
+
+			return nil
+		},
+	})
+}
+
+func TestAccLookerModelSet(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		ProviderFactories: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: `
+				resource "looker_model_set" "test_acc" {
+					name   = "test-acc-model-set"
+					models = ["test_dataset_1", "test_dataset_2", "test_both_datasets"]
+				  }
+				`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("looker_model_set.test_acc", "name", "test-acc-model-set"),
+					resource.TestCheckResourceAttr("looker_model_set.test_acc", "models.#", "3"),
+					testAccModelSet("looker_model_set.test_acc", []string{"test_dataset_1", "test_dataset_2", "test_both_datasets"}),
+				),
+			},
+			{
+				Config: `
+				resource "looker_model_set" "test_acc" {
+					name   = "test-acc-model-set"
+					models = ["test_dataset_1", "test_both_datasets"]
+				  }
+				`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("looker_model_set.test_acc", "name", "test-acc-model-set"),
+					resource.TestCheckResourceAttr("looker_model_set.test_acc", "models.#", "2"),
+					testAccModelSet("looker_model_set.test_acc", []string{"test_dataset_1", "test_both_datasets"}),
+				),
+			},
+		},
+	})
+}
+
+func testAccModelSet(modelSetResource string, expectedModelsSets []string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		modelSetRes, ok := s.RootModule().Resources[modelSetResource]
+		if !ok {
+			return errors.Errorf("Not found: %s", modelSetResource)
+		}
+		if modelSetRes.Primary.ID == "" {
+			return errors.New("model set ID is not set")
+		}
+
+		client := testAccProvider.Meta().(*sdk.LookerSDK)
+
+		modelSet, err := client.ModelSet(modelSetRes.Primary.ID, "", nil)
+		if err != nil {
+			return errors.Wrapf(err, "failed to retrieve model set with id: %v", modelSetRes.Primary.ID)
+		}
+
+		if !slice.UnorderedEqual(*modelSet.Models, expectedModelsSets) {
+			return errors.Errorf("models in model set do not match expected: %v actual: %v", expectedModelsSets, *modelSet.Models)
+		}
+
+		return nil
+	}
+}
