6e4635239c49c3f8c86b57c135c4a3d1976fa991
Mohamed Yusuf <vitzeno@gmail.com>
2022-09-23 16:12:54 +0100

    add functional api checks

--- looker/resource_looker_user_attribute_user_test.go
+++ looker/resource_looker_user_attribute_user_test.go
@@ -5,8 +5,11 @@ import (
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	sdk "github.com/looker-open-source/sdk-codegen/go/sdk/v4"
+	"github.com/pkg/errors"
 	"github.com/resolutionlife/terraform-provider-looker/internal/conv"
+	"github.com/resolutionlife/terraform-provider-looker/internal/slice"
 )
 
 func init() {
@@ -89,8 +92,43 @@ func TestAccLookerUserAttributeUser(t *testing.T) {
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttrSet("looker_user_attribute_user.test_acc", "user_attribute_id"),
 					resource.TestCheckResourceAttrSet("looker_user_attribute_user.test_acc", "user_id"),
-					resource.TestCheckResourceAttr("looker_user_attribute_user.test_acc", "value", "25")),
+					resource.TestCheckResourceAttr("looker_user_attribute_user.test_acc", "value", "25"),
+					testAccRoleUserAttributeUser("looker_user.test_acc"),
+				),
 			},
 		},
 	})
+}
+
+func testAccRoleUserAttributeUser(userResource string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		userRes, ok := s.RootModule().Resources[userResource]
+		if !ok {
+			return errors.Errorf("Not found: %s", userResource)
+		}
+		if userRes.Primary.ID == "" {
+			return errors.New("role user ID is not set")
+		}
+
+		client := testAccProvider.Meta().(*sdk.LookerSDK)
+
+		userAttrs, err := client.UserAttributeUserValues(sdk.RequestUserAttributeUserValues{
+			UserId: userRes.Primary.ID,
+			Fields: conv.P(""),
+		}, nil)
+		if err != nil {
+			return errors.Wrapf(err, "failed to retrieve group with id: %v", userRes.Primary.ID)
+		}
+
+		userIds := []string{}
+		for _, userAttr := range userAttrs {
+			userIds = append(userIds, *userAttr.UserId)
+		}
+
+		if !slice.Contains(userIds, userRes.Primary.ID) {
+			return errors.Errorf("user not found in user attribute users expected: %v actual: %v", userRes.Primary.ID, userIds)
+		}
+
+		return nil
+	}
 }
